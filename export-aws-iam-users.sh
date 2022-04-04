7ecc71b174612c8de35a81674c78919e8cf11028
somaseki <somaseki@somasekis-MacBook-Pro.local>
2022-04-04 10:35:59 +0900

    fix: var name 'usernames' & policy output template

--- export-aws-iam-users.sh
+++ export-aws-iam-users.sh
@@ -1,9 +1,9 @@
-adminusernames=(user1 user2)
-useractions=(get-user list-user-policies list-attached-user-policies )
+usernames=(user1 user2)
+useractions=(get-user list-user-policies list-attached-user-policies list-groups-for-user)
 groupactions=(list-group-policies list-attached-group-policies)
 
-for username in ${adminusernames[@]};
-do
+for username in ${usernames[@]};do
+  echo "$username"
   for action in ${useractions[@]};do
     RES=$(aws iam $action --user-name $username --output table)
     echo "$RES" >> $username.txt
@@ -13,17 +13,14 @@ do
   GROUPNAME=$(echo $(aws iam list-groups-for-user --user-name $username --output json | jq '.Groups[].GroupName') | sed 's/"//g')
   echo $GROUPNAME
   for action in ${groupactions[@]};do
-    echo $username
     RES=$(aws iam $action --group-name $GROUPNAME  --output table)
     echo "$RES" >> $username.txt
     if [ $action = list-attached-group-policies ]; then
       PolicyArns=$(aws iam $action --group-name $GROUPNAME  --output json | jq '.AttachedPolicies[].PolicyArn')
       for arn in ${PolicyArns[@]};do
-        policy=$(aws iam get-policy-version --policy-arn $(echo $arn | sed 's/"//g') --version-id $(aws iam get-policy --policy-arn $(echo $arn | sed 's/"//g') --output json | jq -r '.Policy.DefaultVersionId'))
+        policy=$(aws iam get-policy-version --policy-arn $(echo $arn | sed 's/"//g') --version-id $(aws iam get-policy --policy-arn $(echo $arn | sed 's/"//g') --output json | jq -r '.Policy.DefaultVersionId') --output table)
         echo "$policy" >> $username.txt
       done;
     fi
   done;
 done;
-
-
